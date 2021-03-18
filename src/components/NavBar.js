7169236f6729e6b9508e37ea47b68870ea254d60
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-03-18 02:54:07 -0500

    added NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <h1>Nusaiba Mahmood</h1>
+            <h3>Full Stack Developer</h3>
+        </div>
+    );
+}
+
+export default NavBar;
