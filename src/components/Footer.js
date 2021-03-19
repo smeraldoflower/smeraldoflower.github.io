03f3b6b2f2e4b92c473ff237ee4e36843b14fa61
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-03-19 05:36:04 -0500

    added sticky footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p>Copyright © 2021 Nusaiba Mahmood</p>
+        </div>
+    );
+}
+
+export default Footer;
