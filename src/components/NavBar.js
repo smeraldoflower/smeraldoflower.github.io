cf77a6996c07f17d62232044ce7f04b6d979bf1d
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-03-19 05:10:40 -0500

    installed yarn and icon dependencies + added social links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 
 const NavBar = () => {
     return (
         <div className="navbar">
             <h1>Nusaiba Mahmood</h1>
             <h3>Full Stack Developer</h3>
+            <div className="navbar-socials">
+                <span>
+                    <a target="_blank" href="https://www.linkedin.com/in/iffatnusaibamahmood/">
+                        <FaLinkedin className="navbar-socials-icon" />
+                    </a>
+                </span>
+                <span>
+                    <a target="_blank" href="https://github.com/smeraldoflower">
+                        <FaGithub className="navbar-socials-icon" />
+                    </a>
+                </span>
+                <span>
+                    <a target="_blank" href="https://twitter.com/NusaibaMahmood">
+                        <FaTwitter className="navbar-socials-icon" />
+                    </a>
+                </span>
+            </div>
+
         </div>
     );
 }
